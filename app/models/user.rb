62bd1acf1a180ffddc4e64da7d7cee8a9ba98cc9
cuongdv-1317 <do.viet.cuong@sun-asterisk.com>
2023-10-31 10:27:35 +0700

    Chapter 6: Modeling users

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  const = Rails.application.config_for(:settings)
+  before_save :downcase_email
+
+  validates :name, presence: true, length: {maximum: const["name_length_max"]}
+  validates :email, presence: true, length: {maximum: const["email_length_max"]},
+    format: {with: Regexp.new(const["valid_email_regex"], Regexp::IGNORECASE)}, uniqueness: true
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
